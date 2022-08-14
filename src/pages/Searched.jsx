dc1ca8c8b57ea6ff0d5fb0a493b49ee98e765218
Mohammad Vatankhah <mamadvatan80@gmail.com>
2022-08-14 11:41:07 +0430

    a bug fixed

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Searched = () => {
-  const API = "z";
+  const API = "b173060271ab463981d72c06b38d6b7e";
   const [searched, setSearched] = useState([]);
   let params = useParams();
   const getSearched = async (name) => {
